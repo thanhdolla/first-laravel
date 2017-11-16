bf55c33cdbffe3cddab2cb115851a00c27833d5b
Laptop88 <khuongnguyen.tedx@gmail.com>
2017-11-16 14:06:41 +0700

    hiển thị danh sách sản phẩm

--- app/Http/Controllers/ProductManagementController.php
+++ app/Http/Controllers/ProductManagementController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Loai_san_pham;
+use App\Producer;
 use App\San_pham;
 use Illuminate\Http\Request;
 
@@ -11,27 +13,37 @@ class ProductManagementController extends Controller
     {
 
         $product = San_pham::all();
+
         return view('backend.product.index', compact('product'));
     }
-    public function add(Request $request){
-//        $product = new San_pham();
-//        $product->ten_slide = $request->name;
-//        if ($request->hasFile('image')) {
-//            $file = $request->file('image');
-//            $duoi = $file->getClientOriginalExtension();
-//            if ($duoi != 'jpg' && $duoi != 'png') {
-//                return redirect('admin/slide/add')->with('loi', 'Bạn phải chọn file ảnh');
-//            }
-//
-//            $name = $file->getClientOriginalName();
-//            while (file_exists("upload/slide/.$name")) {
-//                $name = $request->file('image')->getClientOriginalName();
-//            }
-//
-//            $slide->anh_slide = $name;
-//            $file->move('upload/slide/add', $name);
-//            $slide->save();
-//            return redirect('admin/slide/add')->with('thongbao', "Thêm slide thành công");
-//        }
+    public function getAdd()
+    {
+        $cate = Loai_san_pham::all();
+        return view('backend.product.add', compact('cate'));
     }
+    public function add(Request $request){
+        $product = new San_pham();
+        $product->ten_sp = $request->name;
+        $product->loai_spID = $request->category;
+        $product->mo_ta_sp = $request->description;
+        $product->gia_sp = $request->price;
+        if ($request->hasFile('image')) {
+            $file = $request->file('image');
+            $duoi = $file->getClientOriginalExtension();
+            if ($duoi != 'jpg' && $duoi != 'png') {
+                return redirect('admin/product/add')->with('loi', 'Bạn phải chọn file ảnh');
+            }
+
+            $name = $file->getClientOriginalName();
+            while (file_exists("upload/product/.$name")) {
+                $name = $request->file('image')->getClientOriginalName();
+            }
+
+            $product->anh_sp = $name;
+            $file->move('upload/product/add', $name);
+            $product->save();
+            return redirect('admin/product/add')->with('thongbao', "Thêm sản phẩm thành công");
+        }
+    }
+
 }
