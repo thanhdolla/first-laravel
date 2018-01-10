8aa14be3d7be2e7e28701d7ff12d0496f062f801
clitetailor <ducnhatphung@gmail.com>
2018-01-10 12:36:25 +0700

    style login page

--- resources/views/frontend/page/login.blade.php
+++ resources/views/frontend/page/login.blade.php
@@ -1,65 +1,134 @@
 @extends('master')
 @section('content')
-    <div class="inner-header">
-        <div class="container">
-            <div class="pull-left">
-                <h6 class="inner-title">Đăng nhập</h6>
+    <style>
+        [view=login] {
+            display: flex;
+            justify-content: center;
+
+            margin-top: 20px;
+            margin-bottom: 200px;
+        }
+
+        [view=login] .main {
+            width: 800px;
+            font-family: Arial !important;
+        }
+
+        [view=login] .main h1 {
+            font-size: 22px !important;
+            margin-bottom: 15px;
+        }
+
+        [view=login] input {
+            padding: 5px;
+            border: none;
+            outline: none;
+        }
+
+        [view=login] .input .bottom-line {
+            display: flex;
+            justify-content: center;
+
+            width: 100%;
+            height: 2px;
+            background: #eee;
+        }
+
+        
+        [view=login] .input .bottom-line::after {
+            content: '';
+            height: 100%;
+            width: 0px;
+            background: black;
+        }
+
+        [view=login] .input.focus .bottom-line::after {
+            width: 100%;
+        }
+
+        [view=login] .input .bottom-line::after {
+            transition: 0.5s;
+        }
+
+        [view=login] .input input {
+            width: 100%;
+            font-size: 14px !important;
+            padding: 0px;
+            margin: 0px;
+        }
+
+        [view=login] input {
+            padding: 8px !important;
+        }
+
+        [view=login] .input {
+            margin-bottom: 8px;
+        }
+
+        [view=login] button {
+            padding: 5px 20px;
+            font-size: 14px !important;
+            background: black;
+            color: white;
+            border: none;
+            outline: none;
+
+            margin-top: 20px;
+        }
+
+        [view=login] .error {
+            color: red;
+            font-size: 14px !important;
+        }
+    </style>
+
+    <div view="login">
+        <form class="main" action="{{route('login')}}" method="post">
+            <h1>Đăng nhập</h1>
+
+            <input type="hidden" name="_token" value="{{csrf_token()}}"/>
+
+            <div class="input">
+                <input type="text" placeholder="Email Address">
+                <div class="bottom-line"></div>
             </div>
-            <div class="pull-right">
-                <div class="beta-breadcrumb">
-                    <a href="index.html">Home</a> / <span>Đăng nhập</span>
+
+            <div class="input">
+                <input type="password" placeholder="Password">
+                <div class="bottom-line"></div>
+            </div>
+            
+            @if(count($errors)> 0)
+                <div class="error">
+                    @foreach($errors->all() as $err)
+                        {{$err}}<br>
+                    @endforeach
                 </div>
-            </div>
-            <div class="clearfix"></div>
-        </div>
+            @endif
+            @if(Session::has('loi'))
+                <div class="error">
+                    {{Session::get('loi')}}
+                </div>
+            @endif
+            @if(Session::has('thongbao'))
+                <div class="error">
+                    {{Session::get('thongbao')}}
+                </div>
+            @endif
+
+            <button type="submit">Hoàn tất</button>
+        </form>
     </div>
 
-    <div class="container">
-        <div id="content">
+    <script>
+        document.querySelectorAll('[view=login] input').forEach(function (el) {
+            el.addEventListener('focus', function () {
+                el.parentElement.classList.add('focus')
+            })
 
-            <form action="{{route('login')}}" method="post" class="beta-form-checkout">
-                <input type="hidden" name="_token" value="{{csrf_token()}}"/>
-                <div class="row">
-                    <div class="col-sm-3"></div>
-
-                    <div class="col-sm-6">
-                        @if(count($errors)> 0)
-                            <div class="alert alert-danger">
-                                @foreach($errors->all() as $err)
-                                    {{$err}}<br>
-                                @endforeach
-                            </div>
-                        @endif
-                            @if(Session::has('loi'))
-                                <div class="alert alert-danger">
-                                    {{Session::get('loi')}}
-                                </div>
-                            @endif
-                        @if(Session::has('thongbao'))
-                            <div class="alert alert-success">
-                                {{Session::get('thongbao')}}
-                            </div>
-                        @endif
-
-                        <h4>Đăng nhập</h4>
-                        <div class="space20">&nbsp;</div>
-
-
-                        <div class="form-block">
-                            <label for="email">Email address*</label>
-                            <input type="email" name="email" id="email" >
-                        </div>
-                        <div class="form-block">
-                            <label for="phone">Password*</label>
-                            <input type="password" id="pasw" name="password" >
-                        </div>
-                        <div class="form-block">
-                            <button type="submit" class="btn btn-primary">Login</button>
-                        </div>
-                    </div>
-                    <div class="col-sm-3"></div>
-                </div>
-            </form>
-        </div> <!-- #content -->
-    </div> <!-- .container -->
-    @endsection
+            el.addEventListener('blur', function () {
+                el.parentElement.classList.remove('focus')
+            })
+        })
+    </script>
+@endsection
