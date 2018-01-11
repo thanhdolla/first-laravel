05cd38a026fb0624f58572731552bdb44bc25c6f
clitetailor <ducnhatphung@gmail.com>
2018-01-11 17:21:24 +0700

    style product detail

--- resources/views/frontend/page/chitietsanpham.blade.php
+++ resources/views/frontend/page/chitietsanpham.blade.php
@@ -1,24 +1,162 @@
 @extends("master")
 @section("content")
 
-    <div class="inner-header" style="padding-top: 30px;width:90%;padding-top: -10px;">
-        <div class="pull-left" style="padding-left: 100px;font-size:xx-large">Thông tin chi
-            tiết {{$chitiet->ten_sp}}</div>
-        <div class="pull-right" style="font-size:large;">
-            <ol class="breadcrumb">
-                <a href="{{route('trang_chu')}}">Home\</a> <a href="{{route('loai-san-pham',$loai_sp2->id)}}"
-                                                              style="font-size: medium;">{{$loai_sp2->ten_loai_sp}}\</a><span
-                        style="font-size: medium">{{$chitiet->ten_sp}}</span>
-            </ol>
-        </div>
-    </div>
+    <style>
+        [view=detail] {
+            display: flex;
+            flex-flow: column nowrap;
+            align-items: center;
 
-    <div class="container" style="width:90%;border:whitesmoke solid thin;margin:auto;border-top: transparent">
-        <div id="content">
-            <div class="row">
-  @if(Session::has('flag'))
-                        <div class="alert alert-{{Session::get('flag')}}">{{Session::get('message')}}
+            margin-bottom: 100px;
+        }
 
+        [view=detail] .main {
+            width: 1000px;
+            display: flex;
+        }
+
+        [view=detail] .main .left {
+            flex: 0 0 auto;
+            width: 300px;
+        }
+
+        [view=detail] img {
+            object-fit: cover;
+        }
+
+        [view=detail] .main .right {
+            margin-left: 40px;
+            flex: 1 1 auto;
+        }
+
+        [view=detail] h1 {
+            font-family: Lato;
+            font-size: 30px;
+        }
+
+        [view=detail] h2 {
+            font-family: Lato;
+            font-size: 24px;
+        }
+
+        [view=detail] .both {
+            width: 1000px;
+        }
+
+        [view=detail] a,
+        [view=detail] p,
+        [view=detail] b {
+            font-family: Lato !important;
+            font-size: 14px !important;
+        }
+        
+        
+        [view=detail] input {
+            padding: 5px;
+            border: none;
+            outline: none;
+        }
+
+        [view=detail] .input .bottom-line {
+            display: flex;
+            justify-content: center;
+
+            width: 100%;
+            height: 2px;
+            background: #eee;
+        }
+
+        [view=detail] .label {
+            font-size: 12px !important;
+            color: black;
+
+            padding: 0;
+        }
+        
+        [view=detail] .input .bottom-line::after {
+            content: '';
+            height: 100%;
+            width: 0px;
+            background: black;
+        }
+
+        [view=detail] .input.focus .bottom-line::after {
+            width: 100%;
+        }
+
+        [view=detail] .input .bottom-line::after {
+            transition: 0.5s;
+        }
+        
+        [view=detail] .input input,
+        [view=detail] .input textarea {
+            width: 100%;
+            font-family: Arial;
+            font-size: 14px !important;
+            padding: 0px;
+            margin: 0px;
+
+            resize: vertical;
+            
+            padding: 8px !important;
+
+            background: none;
+            outline: none;
+            border: none !important;
+        }
+
+        [view=detail] .input {
+            margin-bottom: 8px;
+        }
+
+        [view=detail] button {
+            padding: 5px 20px;
+            font-size: 14px !important;
+            background: black;
+            color: white;
+            border: none;
+            outline: none;
+
+            margin-top: 20px;
+
+            font-family: Arial;
+            font-size: 14px;
+        }
+
+        [view=detail] .comment {
+            background: rgba(0, 0, 0, 0.1);
+            padding: 5px 20px;
+
+            margin-bottom: 5px;
+        }
+
+        [view=detail] .comment .name {
+            font-family: Lato;
+            font-size: 16px;
+        }
+
+        [view=detail] .comment .date {
+            color: gray;
+        }
+
+        [view=detail] .comment .text {
+            font-family: Arial;
+            font-size: 14px;
+        }
+    </style>
+
+    <div view=detail>
+        <div class="main">
+            <div class="left">
+                <img src="upload/product/add/{{$chitiet->anh_sp}}" alt="{{$chitiet->ten_sp}}" height="400" width="300px">
+            </div>
+
+            <div class="right">
+                <h1>{{$chitiet->ten_sp}}</h1>
+
+                    @if(Session::has('flag'))
+                        <div class="alert alert-{{Session::get('flag')}}">
+                            {{Session::get('message')}}
                         </div>
                     @endif
                     @if(Session::has('thongbao'))
@@ -31,166 +169,136 @@
                             {{Session::get('loi')}}
                         </div>
                     @endif
-                <div class="col-sm-12">
-                    <div class="row">
-                        <div class="col-sm-4">
-                            <img style="height: 500px;width: 350px;margin-top:80px;" src="upload/product/add/{{$chitiet->anh_sp}}"
-                                 alt="">
-                        </div>
-                        <div class="col-sm-8">
-                            <div class="col-sm-1"></div>
-                            <div class="col-sm-7">
-                                <div class="single-item-body">
-                                    <p class="single-item-title"><b><h4>{{$chitiet->ten_sp}}</h4></b></p>
-                                    <div style="padding-top:10px;"></div>
-                                </div>
 
-                                <table class="table table-light">
-                                    <tbody>
-                                    <tr>
-                                        <td>Giá </td>
-                                        <td> <a class="single-item-price">
-                                                <?php $price_new = $chitiet->gia_sp - $chitiet->khuyen_mai ?>
-                                                <?php if ($chitiet->khuyen_mai > 0): ?>
-                                                <a style="text-decoration:line-through;padding-top: 5px;font-size:15px;">
-                                                    <?php echo number_format($chitiet->gia_sp); ?>Đ
-                                                </a>
-                                                <br>
-                                                <a class="ga" style="color:red;padding-top: 10px;font-size: 20px;">
-                                                    <b><?php echo number_format($price_new) ?>Đ</b>
-                                                </a>
-                                                <?php else: ?>
-                                                <a style="color:red;font-size: 20px;"><b><?php echo number_format($chitiet->gia_sp); ?>
-                                                        Đ</b></a>
-                                                <?php endif; ?>
-                                            </a></td>
+                    <br>
 
-                                    </tr>
-                                    <tr>
-                                        <td>Hãng sản xuất</td>
-                                        <td>{{$chitiet->hang_sx}}</td>
+                    <p class="price-before">
+                        <b>Giá:</b>
 
-                                    </tr>
-                                    <tr>
-                                        <td>Ngày sản xuất</td>
-                                        <td>{{$chitiet->ngay_sx}}</td>
+                        <?php $price_new = $chitiet->gia_sp - $chitiet->khuyen_mai ?>
+                        <?php if ($chitiet->khuyen_mai > 0): ?>
+                            <?php echo number_format($chitiet->gia_sp); ?>đ
+                            <br>
+                            <b class="price">
+                                <?php echo number_format($price_new) ?>đ
+                            </b>
+                        <?php else: ?>
+                            <b class="price">
+                                <?php echo number_format($chitiet->gia_sp); ?>đ
+                            </b>
+                        <?php endif; ?>
+                    </p>
 
-                                    </tr>
-                                    <tr>
-                                        <td>Bảo hành</td>
-                                        <td> {{$chitiet->bao_hanh}}</td>
+                    <p>
+                        <b>Hãng sản xuất:</b>
+                        {{$chitiet->hang_sx}}
+                    </p>
 
-                                    </tr>
-                                    </tbody>
-                                </table>
-                                <b><h4>Cấu hình chi tiết:</h4></b>
-                                    <p>
-                                        <?php echo $chitiet->mo_ta_sp?>
-                                    </p>
+                    <p>
+                        <b>Ngày sản xuất:</b>
+                        {{$chitiet->ngay_sx}}
+                    </p>
 
+                    <p>
+                        <b>Bảo hành:</b>
+                        {{$chitiet->bao_hanh}}
+                    </p>
 
-                                <div style="background: red;color:black">
-                                </div>
+                    <br>
 
-                                <div class="single-item-caption" style="padding-top: 5px;">
-                                    <a class="add-to-cart pull-left" href="{{route('addcart',$chitiet->id)}}"
-                                       title="Add to cart"><i class="fa fa-shopping-cart"></i></a>
-                                    <a class="beta-btn primary compare"
-                                       href="addtocompare/{{$chitiet->id}}" title="Add to compare">Add to
-                                        Compare </a>
-                                    <div class="clearfix"></div>
-                                </div>
-                            </div>
-                        </div>
+                    <h2>Cấu hình chi tiết</h2>
 
-                        <div class="clearfix"></div>
-                        <div class="chinhanh" style="padding-top: 50px;">
-                            <h5>Các chi nhánh hiện có sản phẩm</h5>
-                            <div style="padding-left: 10px;">
-                                <?php
-                                foreach($list as $row){
-                                ?>
-                                <a href="chinhanh/{{$row['id']}}"><?php echo $row['ten_chi_nhanh'] ?> - Liên
-                                    hệ: <?php echo $row['sdt']?></a>
-                                <br>
-                                <?php } ?>
-                            </div>
-                        </div>
+                    <p>
+                        <?php echo $chitiet->mo_ta_sp?>
+                    </p>
 
-                        <div class="space40">&nbsp;</div>
-                        <div class="woocommerce-tabs">
-                            @if(Session::has('thongbao'))
-                                <div class="alert alert-success" style="background:#00ced1;width:80%">
-                                    {{Session::get('thongbao')}}
-                                </div>
-                            @endif
-                            @if(Session::has('loi'))
-                                <div class="alert alert-danger" style="width:80%">
-                                    {{Session::get('loi')}}
-                                </div>
-                            @endif
-                            <?php if(Session::has('khach_hang')){ ?>
-                            <ul class="tabs">
-                                <li><a href="#tab-reviews">Bình Luận</a></li>
-                                <div class="panel" id="tab-reviews">
-                                    <form action="binhluan/{{$chitiet->id}}" method="post" class="contact-form">
-                                        <input name="_token" type="hidden" value="{{csrf_token()}}"/>
-                                        <div class="form-block">
-                                            <textarea name="message" style="height:100px;"
-                                                      placeholder="Your Message"></textarea>
-                                        </div>
-                                        <div class="form-block">
-                                            <button type="submit" class="btn btn-success">Gửi bình luận</button>
-                                        </div>
-                                    </form>
+                    <div class="line">
+                        <a href="{{route('addcart',$chitiet->id)}}"></a>
+                        <button>Chọn so sánh</button>
+                        <a href="addtocompare/{{$chitiet->id}}"></a>
+                        <button>Cho vào giỏ hàng</button>
+                    </div>
+            </div>
+        </div>
 
-                                    <table class="table table-striped" style="width: 100%">
-                                        <thead>
+        <div class="both">
+            <br><br>
 
-                                        </thead>
-                                        <tbody>
-                                        @foreach($binhluan as $row)
-                                            <tr>
+            <h2>Các chi nhánh hiện có sản phẩm</h2>
 
-                                                <td style="width:25%;">{{ $row->ten_kh }}<br><span
-                                                            style="color: #9BA2AB">{{ date('d-m-Y', strtotime($row->created_at)) }}</span>
-                                                </td>
-                                                <td>{{ $row->noi_dung }}</td>
+            <?php foreach($list as $row) { ?>
+            <a href="chinhanh/{{$row['id']}}">
+                <?php echo $row['ten_chi_nhanh'] ?> - Liên hệ: <?php echo $row['sdt']?></a>
+            <br>
+            <?php } ?>
 
-                                            </tr>
-                                        @endforeach
-                                        </tbody>
-                                    </table>
-                                </div>
+            <br>
+            
+            <h2>Bình luận</h2>
 
-                            </ul>
-                            <?php } else { ?>
-                            <h5>Bình luận</h5>
-                            <i>(Bạn phải đăng nhập mới có thể giử bình luận)</i>
-                            <table class="table table-striped" style="width: 100%;margin-top: 30px;">
-                                <thead>
+            <?php if(Session::has('khach_hang')){ ?>
+                <form action="binhluan/{{$chitiet->id}}" method="post">
+                    <input name="_token" type="hidden" value="{{csrf_token()}}"/>
 
-                                </thead>
-                                <tbody>
-                                @foreach($binhluan as $row)
-                                    <tr>
-
-                                        <td style="width:25%;">{{ $row->ten_kh }}<br><span
-                                                    style="color: #9BA2AB">{{ date('d-m-Y', strtotime($row->created_at)) }}</span>
-                                        </td>
-                                        <td>{{ $row->noi_dung }}</td>
-
-                                    </tr>
-                                @endforeach
-                                </tbody>
-                            </table>
-                            <?php } ?>
-                        </div>
-
+                    <div class="input">
+                        <div class="label">Bình luận</div>
+                        <input type="text">
+                        <div class="bottom-line"></div>
                     </div>
 
+                    <button type="submit">Gửi bình luận</button>
+                </form>
+
+                <br><br>
+
+                <h2>Danh sách bình luận</h2>
+                
+                <br>
+
+                <div class="comments">
+                    @foreach($binhluan as $row)
+                        <div class="comment">
+                            <div class="name">
+                                {{ $row->ten_kh }}
+                                <span class="date">
+                                    {{ date('d-m-Y', strtotime($row->created_at)) }}
+                                </span>
+                            </div>
+                            <div class="text">{{ $row->noi_dung }}</div>
+                        </div>
+                    @endforeach
                 </div>
-            </div>
-        </div> <!-- #content -->
-    </div> <!-- .container -->
+            <?php } else { ?>
+                <i>Bạn phải đăng nhập mới có thể giử bình luận</i>
+
+                <h2>Danh sách bình luận</h2>
+
+                <div class="comments">
+                    @foreach($binhluan as $row)
+                        <div class="comment">
+                            <div class="name">
+                                {{ $row->ten_kh }}
+                                <span class="date">
+                                    {{ date('d-m-Y', strtotime($row->created_at)) }}
+                                </span>
+                            </div>
+                            <div class="text">{{ $row->noi_dung }}</div>
+                        </div>
+                    @endforeach
+                </div>
+            <?php } ?>
+        </div>
+    </div>
+    
+    <script>
+        document.querySelectorAll('[view=detail] input, [view=contact] textarea').forEach(function (el) {
+            el.addEventListener('focus', function () {
+                el.parentElement.classList.add('focus')
+            })
+
+            el.addEventListener('blur', function () {
+                el.parentElement.classList.remove('focus')
+            })
+        })
+    </script>
 @endsection
