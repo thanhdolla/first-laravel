811b042e9c62fffa4f4b4d2c51988a06749695b3
clitetailor <ducnhatphung@gmail.com>
2018-01-13 01:21:15 +0700

    test: cart page

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -8,15 +8,24 @@ driver.get("http://localhost/Project/public/index")
 test("The title should be 'BKSmart'", function(done) {
   this.timeout(8000)
 
-  driver.wait(until.titleIs('BKSmart'), 4000)
+  driver.wait(until.titleIs("BKSmart"), 4000).then(result => {
+    expect(result).to.eql(true)
+    done()
+  })
+})
+
+test("Move to cart page", function(done) {
+  this.timeout(8000)
 
   driver
-    .getTitle()
-    .then(function(title) {
-      expect(title).to.eql("BKSmart")
-      done()
+    .wait(until.elementLocated(By.css(".cart-icon")), 10000)
+    .click()
+    .then(() => {
+      driver.getCurrentUrl().then(url => {
+        expect(url).to.eql("http://localhost/Project/public/cart")
+        done()
+      })
     })
-  
 })
 
 after(function() {
